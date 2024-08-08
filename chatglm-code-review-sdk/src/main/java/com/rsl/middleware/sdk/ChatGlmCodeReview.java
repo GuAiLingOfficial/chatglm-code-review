8c9350fabe112db74f1d1fded227b0184e6bf440
R100 <ruanshilin100@outlook.com>
2024-08-08 17:31:03 +0800

    feat:检出代码

--- chatglm-code-review-sdk/src/main/java/com/rsl/middleware/sdk/ChatGlmCodeReview.java
+++ chatglm-code-review-sdk/src/main/java/com/rsl/middleware/sdk/ChatGlmCodeReview.java
@@ -1,13 +1,38 @@
 package com.rsl.middleware.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 /**
  * @ description:sdk入口
  * META-INF 用来告知调用该SDK的程序SDK的入口是什么
  * @ author: rsl
  * @ create: 2024-08-06 16:51
  **/
-public class ChatGlmCodeReview{
-    public static void main(String[] args) {
+public class ChatGlmCodeReview {
+    public static void main(String[] args) throws Exception {
         System.out.println("测试执行");
+
+        // 1. 代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码：" + diffCode.toString());
     }
+
 }
+
